3ba0eb5b86d8b0a56ceb811fec9c9bb60efd0919
Khaled301204 <khaledharfoosh2014@gmail.com>
2026-05-04 05:36:30 +0300

    DB connected

--- src/main/java/com/expensia/backend/model/entity/User.java
+++ src/main/java/com/expensia/backend/model/entity/User.java
@@ -1,4 +1,11 @@
 package com.expensia.backend.model.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
 }
